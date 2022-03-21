4527a88c913a29b719b601026d26521382433507
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-21 22:20:46 +0600

    init project

---
+++ src/app/squre/squre.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-squre',
+  template: `
+    <p>
+      squre works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SqureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
